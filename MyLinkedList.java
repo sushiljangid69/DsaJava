5222067102740c42b95fe3e595e57261df66d924
Sushil Jangid <sushiljangid69@gmail.com>
2022-07-19 09:21:10 +0530

    Added functionality of create node at random position and starting.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,15 +3,20 @@ public class MyLinkedList {
     static int count = 0;
     public static void main(String[] args){
         MyLinkedList obj = new MyLinkedList();
-        obj.createNode(1,"First Node");
-        obj.createNode(2,"Second Node");
-        obj.createNode(3,"Third Node");
+        obj.createNodeAtStart(1,"First Node");
+        obj.createNodeAtEnd(2,"Second Node");
+        obj.createNodeAtEnd(3,"Third");
+        obj.createNodeAtEnd(4,"");
+        obj.createNodeAtStart(5,"Fifth");
+        obj.createNodeAtRandom(6,"Random 6",2);
+        obj.createNodeAtEnd(7,"End 7");
+        obj.createNodeAtRandom(8,"Rand 3",3);
         System.out.println("-----------------------------");
 
         obj.traverse();
-        System.out.println(count);
+        System.out.println("Total Nodes are = "+count);
     }
-    void createNode(int id,String name){
+    void createNodeAtEnd(int id, String name){
         if(head == null){
             //there is no node in linked list
             head = new Node(id,name);
@@ -24,6 +29,37 @@ public class MyLinkedList {
             }
             temp.next = new Node(id,name);
             System.out.println(temp.next);
+        }
+        count++;
+    }
+    void createNodeAtRandom(int id,String name,int pos){
+        if(pos > 0 && pos <= count + 1){
+            if(pos == 1){
+                createNodeAtStart(id,name);
+            }else if(pos == count+1){
+                createNodeAtEnd(id,name);
+            }else{
+                Node temp = head; //copy of head
+                for(int i = 1;i < pos-1;i++){
+                    temp = temp.next;
+                }
+                Node temp1 = new Node(id,name);
+                temp1.next = temp.next;
+                temp.next = temp1;
+                count++;
+            }
+        }
+    }
+    void createNodeAtStart(int id,String name){
+        if(head == null){
+            //there is no node in linked list
+            head = new Node(id,name);
+            System.out.println(head);
+        }else{
+            Node temp = new Node(id,name);
+            temp.next = head;
+            head = temp;
+            System.out.println(head);
         }
         count++;
     }
